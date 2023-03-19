6a6e49a4820024392d2bf55a90fb89d1ba2162f0
Your Name <you@example.com>
2023-03-19 11:48:38 +0200

    splashscreen luotu

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -20,79 +20,88 @@ class HomePage extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-              DrawerHeader(
+            DrawerHeader(
               decoration: BoxDecoration(color: Colors.grey.shade700),
               child: CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.grey.shade700,
                 backgroundImage: const AssetImage('assets/pfp_placeholder.jpg'),
               ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.home_rounded),
-                title: const Text('Home'),
-                onTap: () => Navigator.pop(context),
-              ),
-              ListTile(
+            ),
+            ListTile(
+              leading: const Icon(Icons.home_rounded),
+              title: const Text('Home'),
+              onTap: () => Navigator.pop(context),
+            ),
+            ListTile(
                 leading: const Icon(Icons.calendar_month_rounded),
                 title: const Text('Calendar'),
-                onTap: () => null//Navigator.pushNamed(context, route.calendarPage),
-              ),
-              ListTile(
+                onTap: () =>
+                    null //Navigator.pushNamed(context, route.calendarPage),
+                ),
+            ListTile(
                 leading: const Icon(Icons.message_rounded),
                 title: const Text('Messages'),
-                onTap: () => null//Navigator.pushNamed(context, route.messagePage),
-              ),
-              ListTile(
+                onTap: () =>
+                    null //Navigator.pushNamed(context, route.messagePage),
+                ),
+            ListTile(
                 leading: const Icon(Icons.payment_rounded),
                 title: const Text('Salary information'),
-                onTap: () => null//Navigator.pushNamed(context, route.financePage),
-              ),
-              ListTile(
+                onTap: () =>
+                    null //Navigator.pushNamed(context, route.financePage),
+                ),
+            ListTile(
                 leading: const Icon(Icons.person_rounded),
                 title: const Text('Profile'),
-                onTap: () => null//Navigator.pushNamed(context, route.profilePage),
-              ),
-              ListTile(
+                onTap: () =>
+                    null //Navigator.pushNamed(context, route.profilePage),
+                ),
+            ListTile(
                 leading: const Icon(Icons.settings_rounded),
                 title: const Text('Settings'),
-                onTap: () => null//Navigator.pushNamed(context, route.settingsPage),
-              ),
+                onTap: () =>
+                    null //Navigator.pushNamed(context, route.settingsPage),
+                ),
           ],
-      ),
+        ),
       ),
       body: Stack(
         children: <Widget>[
-           Container(
+          Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/homepage_background.jpg'),
+                image: AssetImage('assets/homepage_background.png'),
                 fit: BoxFit.cover,
               ),
             ),
           ),
           Align(
             alignment: const Alignment(0, 0.5),
-            child:ElevatedButton(
+            child: ElevatedButton(
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.orange),
               ),
-              onPressed: () => null, //Navigator.pushNamed(context, route.calendarPage),
+              onPressed: () =>
+                  null, //Navigator.pushNamed(context, route.calendarPage),
               child: const Text('Start your day!'),
             ),
           ),
-           Align(
-              alignment: const Alignment(0, 0.7),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
-                ),
-              onPressed: () => null, //Navigator.pushNamed(context, route.financePage),
+          Align(
+            alignment: const Alignment(0, 0.7),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.orange),
+              ),
+              onPressed: () =>
+                  null, //Navigator.pushNamed(context, route.financePage),
               child: const Text('Salary information'),
             ),
-          ),         
+          ),
         ],
       ),
     );
