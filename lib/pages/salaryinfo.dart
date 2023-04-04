2e491b6d4cf53cfa47e2dd4d576c923c724f256c
t1juko00 <t1juko00@students.oamk.fi>
2023-04-04 12:31:36 +0300

    salaryinfoa eteenpäin

--- lib/pages/salaryinfo.dart
+++ lib/pages/salaryinfo.dart
@@ -1,9 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class SalaryInfo extends StatefulWidget {
   const SalaryInfo({Key? key}) : super(key: key);
-
   @override
   State<SalaryInfo> createState() => _SalaryInfoState();
 }
@@ -11,24 +11,48 @@ class SalaryInfo extends StatefulWidget {
 class _SalaryInfoState extends State<SalaryInfo> {
   final _userInfo = Hive.box('userData');
   final _nameController = TextEditingController();
-
+  FirebaseFirestore overHours = FirebaseFirestore.instance;
+  String? uID;
   bool _editingName = false;
+  Map? _map;
+  List? values;
+@override
+
+   void initState() {
+     super.initState();
+    uID = _userInfo.get("uid");
+    _overHours();
+   }
+
+  void _overHours() async {
+    overHours.collection('/Users/$uID/workTime')
+    .get().then((value) {
+        for (var docSnapshot in value.docs) {
+      _map = docSnapshot.data();
+       print('${_map!['overHours']}');
+      /*values = _map!['overHours'];
+      print(values);*/
+       }
+    }
+    );
+  }
 
   void _toggleEditing(String condition) {
-    if (condition == 'Tuntipalkka:') {
+    if (condition == 'Tuntipalkka') {
       setState(() {
         _editingName = !_editingName;
       });
     }
   }
-
+  
   void _saveName() {
-    _toggleEditing('Tuntipalkka:');
-    _userInfo.put('Tuntipalkka:', _nameController.text);
+    _toggleEditing('Tuntipalkka');
+    _userInfo.put('Tuntipalkka', _nameController.text);
   }
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
         appBar: AppBar(
           title: const Text("Salary information"),
@@ -48,58 +72,58 @@ class _SalaryInfoState extends State<SalaryInfo> {
             child: ListView(
               children: [
                 const SizedBox(
-                  height: 15,
+                  height: 25,
                 ),
                 Row(
-                  children: const [
+                  children:  const [
                     Text(
                       "Tehtävänimike: Putkiasentaja",
-                      style: TextStyle(
-                        backgroundColor: Colors.orange,
-                        fontSize: 25,
-                      ),
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
                     ),
                   ],
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 40,
                 ),
                 Row(
                   children: const [
                     Text(
                       "Viikkotyöaika: 38h",
                       style: TextStyle(
-                        backgroundColor: Colors.orange,
-                        fontSize: 25,
-                      ),
+                      fontSize: 20
+                    ),
                     ),
                   ],
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 40,
                 ),
                 Row(
-                  children: [
-                    const Text('Tuntipalkka:'),
+                  children:  [
+                    const Text("Tuntipalkka /h : ",
+                    style: TextStyle(
+                      fontSize: 20
+                    ),),
                     const SizedBox(
                       width: 20,
                     ),
-                    Expanded(
-                      child: TextFormField(
-                          controller: _nameController,
-                          enabled: _editingName,
-                          decoration: InputDecoration(
-                            hintText: _userInfo.get('Tuntipalkka:'),
-                            hintStyle: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          onSaved: (value) {
-                            _nameController.text = value!;
-                          }),
-                    ),
-                    IconButton(
-                      onPressed: () => _toggleEditing('Tuntipalkka:'),
+                    Expanded(child: TextFormField(
+                      controller: _nameController,
+                      enabled: _editingName,
+                      decoration: InputDecoration(
+                        hintText: _userInfo.get('Tuntipalkka'),
+                        hintStyle: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onSaved: (value) {
+                        _nameController.text = value!;
+                      }),
+                      ),
+                      IconButton(
+                      onPressed: () => _toggleEditing('Tuntipalkka'),
                       icon: _editingName
                           ? const Icon(Icons.cancel)
                           : const Icon(Icons.edit),
@@ -112,32 +136,50 @@ class _SalaryInfoState extends State<SalaryInfo> {
                   ],
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 40,
                 ),
                 Row(
-                  children: const [
+                  children:  const [
                     Text(
                       "Vuosilomat: 30 päivää",
                       style: TextStyle(
-                        backgroundColor: Colors.orange,
-                        fontSize: 25,
-                      ),
+                      fontSize: 20
+                    ), 
                     ),
                   ],
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 40,
                 ),
-                Row(
-                  children: const [
+                 Row(
+                  children:  [
                     Text(
-                      'Työtunnit:',
-                      style: TextStyle(
-                        backgroundColor: Colors.orange,
-                        fontSize: 25,
-                      ),
+                      'Ylityötunnit: ',
+                     style: TextStyle(
+                      fontSize: 20
                     ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                   
+                    Text(
+                      "",
+                      style: 
+                      TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                   
                   ],
+                ),
+                const SizedBox(
+                  height: 300,
+                ),
+                ElevatedButton(
+                onPressed: () {
+                },
+                child: const Text("Työhistoria")
                 ),
               ],
             ),
